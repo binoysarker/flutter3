d9e815b0dbd054b1742e347303c995f3deff1656
binoysarker <binoy3412@gmail.com>
2022-07-25 18:27:15 +0600

    initial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Container(
+        child: Card(
+          child: Row(
+            children: [Text('hello world')],
+          ),
+        ),
+      ),
+    );
+  }
+}
